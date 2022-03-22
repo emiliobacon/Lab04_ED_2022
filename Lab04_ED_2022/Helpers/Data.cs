7f61c989d8f67bc38ddd4f10b23a7b37c56eb886
EMILIO ANTONIO BARILLAS CONTRERAS <eabarillasco@correo.url.edu.gt>
2022-03-21 20:20:23 -0600

    added singleton antipattern and patient model

---
+++ Lab04_ED_2022/Helpers/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab04_ED_2022.Helpers
+{
+    public class Data
+    {
+        private static Data _instance = null;
+        public static Data Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Data();
+                }
+                return _instance;
+            }
+        }
+    }
+}
---
+++ Lab04_ED_2022/Models/ModeloPaciente.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab04_ED_2022.Models
+{
+    public class ModeloPaciente
+    {
+//      Nombre para mostrar al momento de visualizar pacientes
+        [Required]
+        public string Nombres { get; set; }
+//      0 hombre 1 mujer
+        [Required]
+        public bool Genero { get; set; }
+        [Required]
+        public int Edad { get; set; }
+        [Required]
+        public string Especializacion { get; set; }
+//      0 ambulancia 1 asistido
+        [Required]
+        public bool Ingreso { get; set; }
+   
+    }
+}
